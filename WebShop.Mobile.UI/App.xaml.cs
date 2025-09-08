6cccb96209075bada0b7eabd17d2cf266c8c60a2
HFGF <HFGF@DESKTOP-3FAJ25G>
2025-09-08 14:24:47 +0200

    init WebShop projects

---
+++ WebShop.Mobile.UI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace WebShop.Mobile.UI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
